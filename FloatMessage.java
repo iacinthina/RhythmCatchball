596f931a8fca105ca3c0b422c2dc511048890fce
mimseong <parkseongmin98@gmail.com>
2019-05-08 15:50:54 +0900

    Update FloatMessage.java

--- FloatMessage.java
+++ FloatMessage.java
@@ -5,14 +5,19 @@ public class FloatMessage
 	public float vspeed;
 	public float friction;
 	
-	public FloatMessage(float x, float y, boolean isMove)
+	public FloatMessage(float x, float y)
 	{
-		if(isMove == true)
-			this.vspeed = 5;
+		this.framesLeft = 15;
+		
+		
 	}
 	
 	public void Update()
 	{
+		y + vspeed
+		vs - fr
 		
+		
+		vs -= sign(vs)*min(fr, avs(vs)) //양수 1 음수 -1
 	}
 }
