62055bf10a12d1fa1aa866a62c1098fa8405b05d
mimseong <parkseongmin98@gmail.com>
2019-05-08 14:58:27 +0900

    안에 틀 만듬

--- Gameobj.java
+++ Gameobj.java
@@ -1,4 +1,32 @@
-
 public class Gameobj {
-
+	private float xpos;
+	private float ypos;
+	private int imageIndex;
+	private int xoffset;
+	private int yoffset;
+	
+	public void Draw(void)
+	{
+		
+	}
+	public void Update(void)
+	{
+		
+	}
+	public void Destroy(void)
+	{
+		
+	}
+	public void ShiftPos(float x, float y)
+	{
+		
+	}
+	public void SetPos(float x, float y)
+	{
+		
+	}
+	public void SetOffset(int x, int y)
+	{
+		
+	}
 }
