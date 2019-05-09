207ef856516c901c9fcb3a629b5cf97e3f150643
mimseong <parkseongmin98@gmail.com>
2019-05-09 21:37:34 +0900

    Ball 클래스 초기화부분

--- src/Ball.java
+++ src/Ball.java
@@ -8,12 +8,23 @@ public class Ball {
 	public Player toward;
 	
 	public Ball() {
-		
+		//framesTotal :총 몇프레임 가야하는지
+		//
 	}
 	
 	public void Update() {
+		framesLeft--;
+		
 		
 	}
+	
+	public void ResetValue(int x, int y, int total) {
+		xstart = x;
+		ystart = y;
+		framesTotal = total;
+		framesLeft = framesTotal;
+	}
+	
 	public int Judgement() {
 		
 	}
