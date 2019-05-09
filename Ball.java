2c6a29c6ae347292eaf59e611c488f97fd272406
mimseong <parkseongmin98@gmail.com>
2019-05-09 17:54:28 +0900

    Ball 클래스 틀만 잡아 놓음

---
+++ Ball.java
@@ -0,0 +1,20 @@
+
+public class Ball {
+	public int framesLeft;
+	public int framesTotal;
+	public int caughtHold;
+	public float xstart;
+	public float ystart;
+	public Player toward;
+	
+	public Ball() {
+		
+	}
+	
+	public void Update() {
+		
+	}
+	public int Judgement() {
+		
+	}
+}
