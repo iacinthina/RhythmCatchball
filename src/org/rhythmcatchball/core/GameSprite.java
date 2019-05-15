32897cffeb0122f91e11feadf69c56ef4e8c8e2b
Simon Lim <kkklim1121@hotmail.com>
2019-05-15 15:40:51 +0900

    game전체 실행 test해서 수정함

--- src/org/rhythmcatchball/core/GameSprite.java
+++ src/org/rhythmcatchball/core/GameSprite.java
@@ -1,6 +1,7 @@
 package org.rhythmcatchball.core;
 
 import java.awt.Image;
+import java.awt.image.ImageObserver;
 import java.util.HashMap;
 import java.io.BufferedReader;
 import java.io.File;
@@ -41,48 +42,45 @@ public class GameSprite {
 	 * comment : 
 	 * @throws IOException 
 	 */
-	public static boolean loadImages() throws IOException {
+	public static boolean loadImages(ImageObserver arg0) {
 		/*
 		 * 파일을 돌아가며 설정. Image[] sprite에 넣는다.
 		 */
-		GameManager gm = GameManager.getref();
 		Image loadimg;
 		GameSprite spr;
-		String[] fnameList = {"sprites/spr_message_", ""};
-		int[] subimg = {11, 0};
-		int i, sprType;
 		File file = new File("sprites/spritesName.txt");
+		boolean result = false;
 		//파일 directory 바꿔줘야함
 		
-		FileReader filereader= new FileReader(file);
+		FileReader filereader;
 		
-		BufferedReader bufferedreader = new BufferedReader(filereader);
+		sprites = new HashMap<String, GameSprite>();
+		try {
+			filereader = new FileReader(file);
 		
-		while(true)
-		{
-			String filename = bufferedreader.readLine();
-			if(filename == null)
-				break;
-			loadimg = new ImageIcon(filename + ".png").getImage();//이미지 파일 로드시 오류처리? 필요?
-			spr = new GameSprite(loadimg.getWidth(gm)/2, loadimg.getHeight(gm)/2, loadimg);
-			sprites.put(filename, spr);
-		}
-		//파일로부터 파일명 읽어서 그대로 키값으로 활용, 해쉬맵에 추가 
-		
-		/*이전에 있던
-		for(sprType = 0; sprType < 1; sprType++) {
-			for(i=0; i<subimg[sprType]; i++)
+			BufferedReader bufferedreader = new BufferedReader(filereader);
+			
+			while(true)
 			{
-				loadimg = new ImageIcon(fnameList[sprType] +i+ ".png").getImage();
-				spr = new GameSprite(loadimg.getWidth(gm)/2, loadimg.getHeight(gm)/2, loadimg);
-				sprites.put("" + i, spr);
+				String filename = bufferedreader.readLine();
+				if(filename == null)
+					break;
+				loadimg = new ImageIcon("sprites/"+filename + ".png").getImage();//이미지 파일 로드시 오류처리? 필요?
+				spr = new GameSprite(loadimg.getWidth(arg0)/2, loadimg.getHeight(arg0)/2, loadimg);
+				System.out.println("spr = "+spr+" fname = "+filename);
+				sprites.put(filename, spr);
 			}
+			//파일로부터 파일명 읽어서 그대로 키값으로 활용, 해쉬맵에 추가 
+			result = true;
+			bufferedreader.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		*/
-		
-		
-		
-		return false;//성공시 true
+		return result;//성공시 true
 	}
 	
 	/**
@@ -91,12 +89,13 @@ public class GameSprite {
 	 * comment : 
 	 */
 	//해당 키값에 대해 해쉬맵에 value존재하는지 확인, 있다면 sprite값 리턴, 없으면 null 리턴 
-	public static GameSprite getImage(String key) {
+	public static GameSprite get(String key) {
 		GameSprite spr = null;
 		
 		spr = sprites.get(key);
 		
-		if(!sprites.containsKey(key)) {			return null;
+		if(!sprites.containsKey(key)) {
+			return null;
 		}
 		
 		return spr;
