c4e30f10d7ed70407208d873807ef5e5d7770472
kimbeomjoon <myas1717@naver.com>
2019-06-06 16:35:13 +0900

    else indentation

--- src/org/rhythmcatchball/gameplay/RoundManager.java
+++ src/org/rhythmcatchball/gameplay/RoundManager.java
@@ -10,79 +10,78 @@ public class RoundManager extends GameObj {
 	private int playtime;
 	private int timelimit;
 	private int beatcount;
-	
+
 	private ArrayList<Ball> tossable;
 	private Ball tossed;
 	private ArrayList<Ball> taken;
-	
+
 	private ArrayList<Controller> ctrls;
-	
+
 	private int signal_skill;
 	private int signal_toss;
 	private int signal_grab;
 	private int signal_great;
 	private int signal_reversal;
-	
-	
+
 	public RoundManager() {
 		entry = new ArrayList<Player>();
 		playtime = -3;
 		timelimit = 60;
 		beatcount = 0;
-		
+
 		tossable = new ArrayList<Ball>();
 		tossed = null;
 		taken = new ArrayList<Ball>();
-		
+
 		ctrls = new ArrayList<Controller>();
-		
+
 		signal_skill = 0;
 		signal_toss = 0;
 		signal_grab = 0;
 		signal_great = 0;
 		signal_reversal = 0;
 	}
-	
+
 	public static GameObj create(float xpos, float ypos) {
 		RoundManager instance = null;
-		
+
 		instance = new RoundManager();
 		instance.xpos = xpos;
 		instance.ypos = ypos;
-		
+
 		register(instance);
-		
+
 		return instance;
 	}
-	
+
 	@Override
 	public void update() {
 		int beatrate = getBeatrate();
-		
+
 		if (++beatcount >= beatrate) {
 			beatcount = 0;
 			onBeat();
-			System.out.println("playtime = "+playtime);
+			System.out.println("playtime = " + playtime);
 			if (tossable.isEmpty()) {
-				for(Player p : entry)
+				for (Player p : entry)
 					p.onBeat();
-				for(Ball b : taken)
+				for (Ball b : taken)
 					b.onBeat();
 			}
 		}
 		signal_toss();
-		
-		//짝수일때는 0부터, 홀수일때는 1부터
+
+		// 짝수일때는 0부터, 홀수일때는 1부터
 		Controller c;
 		int i, order, cnt;
-		i = (beatcount % 2 == 0)? 0 : ctrls.size()-1;
-		order = (i == 0)? 1 : -1;
+		i = (beatcount % 2 == 0) ? 0 : ctrls.size() - 1;
+		order = (i == 0) ? 1 : -1;
 		cnt = 0;
-		//iteration
-		for(;cnt < ctrls.size(); cnt++, i += order) {
+		// iteration
+		for (; cnt < ctrls.size(); cnt++, i += order) {
 			c = ctrls.get(i);
 			if (c == null) {
-				System.out.println("c = ctrls.get("+i+")");
+				System.out.println("c = ctrls.get(" + i + ")");
 				continue;
 			}
 			if (c.catchCheck() && !tossable.isEmpty()) {
@@ -94,13 +93,13 @@ public class RoundManager extends GameObj {
 					toss.reset(toss.xpos, toss.ypos, 0.5f, target, 0f);
 					taken.add(toss);
 				}
-				System.out.println("toss = "+toss);
+				System.out.println("toss = " + toss);
 			}
 			c.update(beatcount);
 		}
-		
+
 	}
-	
+
 	@Override
 	public void onBeat() {
 		if (tossed != null) {
@@ -113,7 +112,7 @@ public class RoundManager extends GameObj {
 			tossed = b;
 		}
 		if (playtime == 0) {
-			for(Player player : entry) 
+			for (Player player : entry)
 				player.setActive(true);
 		}
 		if (playtime >= timelimit) {
@@ -121,7 +120,7 @@ public class RoundManager extends GameObj {
 		}
 		playtime++;
 	}
-	
+
 	public void initPlayers() {
 		int xdiff = (int) (GameManager.getref().getResWidth() * 0.4);
 		int ydiff = (int) (GameManager.getref().getResHeight() * 0.3);
@@ -135,11 +134,11 @@ public class RoundManager extends GameObj {
 		P1.setActive(false);
 		P2.setActive(false);
 	}
-	
+
 	private void endGame() {
 		int score, highest = -1;
 		boolean draw = false;
-		for(Player player : entry) {
+		for (Player player : entry) {
 			score = player.getScore();
 			if (score == highest)
 				draw = true;
@@ -147,23 +146,23 @@ public class RoundManager extends GameObj {
 				highest = score;
 			}
 		}
-		
+
 		int msgtime = 1000;
 		float vspd = -5f, fric = 0.3f;
-		for(Player player : entry) {
+		for (Player player : entry) {
 			if (player.getScore() == highest) {
 				if (draw)
 					FloatMessage.create(player.xpos, player.ypos, "endgame_draw", msgtime, vspd, fric);
-				else 
+				else
 					FloatMessage.create(player.xpos, player.ypos, "endgame_win", msgtime, vspd, fric);
-			} else
+			} else {
 				FloatMessage.create(player.xpos, player.ypos, "endgame_lose", msgtime, vspd, fric);
-			
+			}
 			player.setActive(false);
 		}
 		setActive(false);
 	}
-	
+
 	public Player getEntry(int index) {
 		if (entry.isEmpty()) {
 			initPlayers();
@@ -171,18 +170,19 @@ public class RoundManager extends GameObj {
 		}
 		return entry.get(index);
 	}
-	
+
 	public void addController(Controller ctrl) {
-		if (ctrl != null) ctrls.add(ctrl);
+		if (ctrl != null)
+			ctrls.add(ctrl);
 	}
-	
+
 	private void signal_toss() {
-		for(Player p : entry) {
+		for (Player p : entry) {
 			if (p.countBall() == 3)
 				signal_toss++;
 		}
 		if (signal_toss > 80 && signal_toss < 100 && playtime < 300) {
-			//lettersign=3 sign_toss=100 alarm[2]=beat*3
+			// lettersign=3 sign_toss=100 alarm[2]=beat*3
 		}
 	}
 }
